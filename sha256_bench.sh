732cbccaa7f16b5283b65fd848b58fc8917c7953
PatStiles <33334338+PatStiles@users.noreply.github.com>
2024-03-05 21:30:15 -0300

    Update sha256_bench.sh

--- sha256_bench.sh
+++ sha256_bench.sh
@@ -17,10 +17,10 @@ echo "Generating Proof"
 cairo-run     --program=../../sha256/sha256_compiled.json     --layout=recursive --program_input=../../$1.json --air_public_input=../../sha256/public_input.json     --air_private_input=../../sha256/private_input.json     --trace_file=../../sha256/trace.json     --memory_file=../../sha256/memory.json     --print_output     --proof_mode
 
 echo "Benchmarking Stone Prover constraining Keccak on $1 bytes"
-hyperfine './cpu_air_prover     --out_file=../../sha256/sha256_proof.json     --private_input_file=../../sha256/private_input.json --public_input_file=../../sha256/public_input.json     --prover_config_file=../../sha256/cpu_air_prover_config.json --parameter_file=../../sha256/cpu_air_params.json     --generate_annotations' --show-output > air_prover.txt
+hyperfine './cpu_air_prover     --out_file=../../sha256/sha256_proof.json     --private_input_file=../../sha256/private_input.json --public_input_file=../../sha256/public_input.json     --prover_config_file=../../sha256/cpu_air_prover_config.json --parameter_file=../../sha256/cpu_air_params.json     --generate_annotations' --show-output > sha256_$1_air_prover.txt
 
 echo "Benchmarking Stone Air Verifier for Keccak on $1 bytes"
-hyperfine './cpu_air_verifier --in_file=../../sha256/sha256_proof.json' --show-output > air_verifier.txt
+hyperfine './cpu_air_verifier --in_file=../../sha256/sha256_proof.json' --show-output > sha256_$1_air_verifier.txt
 
 cd ../../
 cd cairo-lang
@@ -30,5 +30,5 @@ echo "Compiling Cairo Verifier for recursive layout"
 cairo-compile --cairo_path=./src src/starkware/cairo/cairo_verifier/layouts/all_cairo/cairo_verifier.cairo --output cairo_verifier.json --no_debug_info
 
 echo "Benchmarking Cairo Verifier for Keccak on $1 bytes"
-hyperfine 'cairo-run --program=cairo_verifier.json --layout=recursive --program_input=cairo_verifier_input.json --trace_file=cairo_verifier_trace.json --memory_file=cairo_verifier_memory.json --print_output' --show-output > cairo_verifier.txt
+hyperfine 'cairo-run --program=cairo_verifier.json --layout=recursive --program_input=cairo_verifier_input.json --trace_file=cairo_verifier_trace.json --memory_file=cairo_verifier_memory.json --print_output' --show-output > sha256_$1_cairo_verifier.txt
 cd ..
