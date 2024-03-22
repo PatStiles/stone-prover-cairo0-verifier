1e83a988a827de736ceb8742fd7b2cfe334a67b1
PatStiles <pdstiles78@gmail.com>
2024-03-22 17:34:20 +0000

    rm input files and integrate gen_input in bench

--- bench.sh
+++ bench.sh
@@ -5,8 +5,12 @@ set -o xtrace
 # Exit on error.
 set -e
 
+chmod +x gen_input.sh
 chmod +x sha256_bench.sh
 chmod +x keccak_bench.sh
 
+data_len=$(($1 / 32))
+
+./gen_input.sh "$data_len"
 ./sha256_bench.sh $1
 ./keccak_bench.sh $1
