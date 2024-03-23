0bfddddb1df0af6c891d7b83459cec7c6a881c4d
PatStiles <pdstiles78@gmail.com>
2024-03-23 17:40:52 +0000

    update bench scripts to run one round + nits

--- keccak_bench.sh
+++ keccak_bench.sh
@@ -14,10 +14,10 @@ echo "Generating Proof"
 cairo-run     --program=../../keccak/keccak_compiled.json     --layout=recursive --program_input=../../$1.json --air_public_input=../../keccak/public_input.json     --air_private_input=../../keccak/private_input.json     --trace_file=../../keccak/trace.json     --memory_file=../../keccak/memory.json     --print_output     --proof_mode
 
 echo "Benchmarking Stone Prover constraining Keccak on $1 bytes"
-hyperfine './cpu_air_prover     --out_file=../../keccak/keccak_proof.json     --private_input_file=../../keccak/private_input.json --public_input_file=../../keccak/public_input.json     --prover_config_file=../../keccak/cpu_air_prover_config.json --parameter_file=../../keccak/cpu_air_params.json     --generate_annotations' --show-output > keccak_$1_air_prover.txt
+hyperfine -r 1 './cpu_air_prover     --out_file=../../keccak/keccak_proof.json     --private_input_file=../../keccak/private_input.json --public_input_file=../../keccak/public_input.json     --prover_config_file=../../keccak/cpu_air_prover_config.json --parameter_file=../../keccak/cpu_air_params.json     --generate_annotations' --show-output > keccak_$1_air_prover.txt
 
 echo "Benchmarking Stone Air Verifier for Keccak on $1 bytes"
-hyperfine './cpu_air_verifier --in_file=../../keccak/keccak_proof.json' --show-output > keccak_$1_air_verifier.txt
+hyperfine -r 1 './cpu_air_verifier --in_file=../../keccak/keccak_proof.json' --show-output > keccak_$1_air_verifier.txt
 
 cd ../../
 cd cairo-lang
@@ -27,5 +27,5 @@ echo "Compiling Cairo Verifier for recursive layout"
 cairo-compile --cairo_path=./src src/starkware/cairo/cairo_verifier/layouts/all_cairo/cairo_verifier.cairo --output cairo_verifier.json --no_debug_info
 
 echo "Benchmarking Cairo Verifier for Keccak on $1 bytes"
-hyperfine 'cairo-run --program=cairo_verifier.json --layout=recursive --program_input=cairo_verifier_input.json --trace_file=cairo_verifier_trace.json --memory_file=cairo_verifier_memory.json --print_output' --show-output > keccak_$1_cairo_verifier.txt
+hyperfine -r 1 'cairo-run --program=cairo_verifier.json --layout=recursive --program_input=cairo_verifier_input.json --trace_file=cairo_verifier_trace.json --memory_file=cairo_verifier_memory.json --print_output' --show-output > keccak_$1_cairo_verifier.txt
 cd ..
--- sha256_bench.sh
+++ sha256_bench.sh
@@ -7,17 +7,17 @@ set -e
 
 cd stone-prover/e2e_test
 
-echo "Compiling Keccak Benchmark"
+echo "Compiling Sha256 Benchmark"
 cairo-compile ../../sha256/sha256.cairo --output ../../sha256/sha256_compiled.json --proof_mode
 
 echo "Generating Proof"
 cairo-run     --program=../../sha256/sha256_compiled.json     --layout=recursive --program_input=../../$1.json --air_public_input=../../sha256/public_input.json     --air_private_input=../../sha256/private_input.json     --trace_file=../../sha256/trace.json     --memory_file=../../sha256/memory.json     --print_output     --proof_mode
 
-echo "Benchmarking Stone Prover constraining Keccak on $1 bytes"
-hyperfine './cpu_air_prover     --out_file=../../sha256/sha256_proof.json     --private_input_file=../../sha256/private_input.json --public_input_file=../../sha256/public_input.json     --prover_config_file=../../sha256/cpu_air_prover_config.json --parameter_file=../../sha256/cpu_air_params.json     --generate_annotations' --show-output > sha256_$1_air_prover.txt
+echo "Benchmarking Stone Prover constraining Sha256 on $1 bytes"
+hyperfine -r 1 './cpu_air_prover     --out_file=../../sha256/sha256_proof.json     --private_input_file=../../sha256/private_input.json --public_input_file=../../sha256/public_input.json     --prover_config_file=../../sha256/cpu_air_prover_config.json --parameter_file=../../sha256/cpu_air_params.json     --generate_annotations' --show-output > sha256_$1_air_prover.txt
 
-echo "Benchmarking Stone Air Verifier for Keccak on $1 bytes"
-hyperfine './cpu_air_verifier --in_file=../../sha256/sha256_proof.json' --show-output > sha256_$1_air_verifier.txt
+echo "Benchmarking Stone Air Verifier for Sha256 on $1 bytes"
+hyperfine -r 1 './cpu_air_verifier --in_file=../../sha256/sha256_proof.json' --show-output > sha256_$1_air_verifier.txt
 
 cd ../../
 cd cairo-lang
@@ -26,6 +26,6 @@ jq '{ proof: . }' ../sha256/sha256_proof.json > cairo_verifier_input.json
 echo "Compiling Cairo Verifier for recursive layout"
 cairo-compile --cairo_path=./src src/starkware/cairo/cairo_verifier/layouts/all_cairo/cairo_verifier.cairo --output cairo_verifier.json --no_debug_info
 
-echo "Benchmarking Cairo Verifier for Keccak on $1 bytes"
-hyperfine 'cairo-run --program=cairo_verifier.json --layout=recursive --program_input=cairo_verifier_input.json --trace_file=cairo_verifier_trace.json --memory_file=cairo_verifier_memory.json --print_output' --show-output > sha256_$1_cairo_verifier.txt
+echo "Benchmarking Cairo Verifier for Sha256 on $1 bytes"
+hyperfine -r 1 'cairo-run --program=cairo_verifier.json --layout=recursive --program_input=cairo_verifier_input.json --trace_file=cairo_verifier_trace.json --memory_file=cairo_verifier_memory.json --print_output' --show-output > sha256_$1_cairo_verifier.txt
 cd ..
